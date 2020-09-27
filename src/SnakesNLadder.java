bf4a9dee006863d2461db019feda4723e91e0523
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 03:05:23 +0530

    Added to Repeat till Player gets 100

--- src/SnakesNLadder.java
+++ src/SnakesNLadder.java
@@ -1,12 +1,55 @@
+import java.util.Random;
 
 public class SnakesNLadder {
+	
+	static final int ladder = 1, snake = 2;
+	static int pos = 0;
+
+	public static int getNumber(int a){
+		Random rand = new Random();
+		int max =a;
+		int num = rand.nextInt(max);
+
+		return num;
+	}
+
+	public static int checkOptions() {
+		int num = getNumber(3);
+		return num;
+	}
+
+	public static void movement(int option, int steps){
+
+		switch (option){
+			case ladder:	
+				pos += steps;
+				break;
+			case snake:
+				if(pos -steps >= 0)
+					pos -= steps;
+				else
+					pos = 0;
+				break;
+			default: 
+		}
+
+		System.out.println("The total pos is " + pos);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Snakes And Ladder Game - Single Player");
-		int init_pos =0;
+		
+		while(pos <= 100) {
+			if(pos == 100)
+				break;
+			int num = getNumber(6) + 1;
+			System.out.println("The number obtained is: " + num);
 
+			int option = checkOptions();
+			System.out.println("The option obtained is: " + option);	
+			movement(option, num);
+		}
 	}
 
 }
