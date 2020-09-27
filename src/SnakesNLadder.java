c3249835a74469f97b808886a052b4eba79103ed
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 02:52:26 +0530

    Added to Obtain number on rolling the dice

--- src/SnakesNLadder.java
+++ src/SnakesNLadder.java
@@ -1,12 +1,15 @@
+import java.util.Random;
 
 public class SnakesNLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Snakes And Ladder Game - Single Player");
-		int init_pos =0;
-
+		Random rand = new Random();
+		int max = 6;
+		int num = rand.nextInt(max) + 1;
+		
+		System.out.println("The number obtained on rolling the dice is: "+ num);
 	}
 
 }
