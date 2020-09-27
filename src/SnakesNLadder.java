b76790b000a915784a30e3be6e80a39b5495f177
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 02:57:13 +0530

    Added to Check options for movement of player

--- src/SnakesNLadder.java
+++ src/SnakesNLadder.java
@@ -1,11 +1,48 @@
+import java.util.Random;
 
 public class SnakesNLadder {
 
+	static final int ladder = 1, snake = 2;
+	static int moves = 0;
+	
+	public static int getNumber(int a){
+		Random rand = new Random();
+		int max =a;
+		int num = rand.nextInt(max);
+
+		return num;
+	}
+
+	public static int checkOptions() {
+		int num = getNumber(3);
+		return num;
+	}
+	
+	public static void movement(int option, int steps){
+
+		switch (option){
+			case ladder:	
+				moves += steps;
+				break;
+			case snake:
+				if(moves -steps >= 0)
+					moves -= steps;
+				break;
+			default: 
+		}
+
+		System.out.println("The total moves is " + moves);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Snakes And Ladder Game - Single Player");
-		int init_pos =0;
+		int num = getNumber(6) + 1;
+		System.out.println("The number obtained is: " + num);
+
+		int option = checkOptions();
+		System.out.println("The option obtained is: " + option);	
+		movement(option, num);
 
 	}
 
