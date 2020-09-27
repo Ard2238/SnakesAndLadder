db9b5f7860bc004a239acd4ab76e5366d3487178
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 02:47:45 +0530

    Snakes And Ladder Game - With Single Player

---
+++ src/SnakesNLadder.java
@@ -0,0 +1,12 @@
+
+public class SnakesNLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Snakes And Ladder Game - Single Player");
+		int init_pos =0;
+
+	}
+
+}
